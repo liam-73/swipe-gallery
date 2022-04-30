77b4cd74b885a4bc84ca2fcb2ed707fb90fcc5fd
liam <minthantnyi1212@gmail.com>
2022-04-30 18:13:34 +0630

    first commit

---
+++ gallery.js
@@ -0,0 +1,9 @@
+new Swiper(".swiper-container", {
+    speed: 500,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
